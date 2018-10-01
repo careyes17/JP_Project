68e318e8c2b029702cc97c56ab6c0bbfbd732f18
careyes17 <careyes4820@eagle.fgcu.edu>
2018-10-01 09:08:12 -0400

    Converted to Java Project, completed steps 4-6

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,26 +6,11 @@
 
 package sample;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 
-public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        //Title of application window
-        primaryStage.setTitle("Hello World");
-        //Sets size of application window
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
+public class Main {
 
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+  public static void main(String[] args) {
+
+  }
 }
